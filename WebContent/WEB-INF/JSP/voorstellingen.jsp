830647a8f4140609609ee991d493841b259e89cb
OkkeReumers <okkereumers@hotmail.com>
2015-06-18 10:17:27 +0200

    18/06

---
+++ WebContent/WEB-INF/JSP/voorstellingen.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<%@taglib uri='http://vdab.be/tags' prefix='vdab'%>
+<%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
+<!doctype html>
+<html>
+<head>
+	<vdab:head title='Voorstellingen'/>
+</head>
+<body>
+<h1>Test</h1>
+</body>
+</html>
--- src/be/vdab/servlets/Voorstellingen.java
+++ src/be/vdab/servlets/Voorstellingen.java
@@ -1,37 +1,30 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class voorstellingen
- */
-@WebServlet("/voorstellingen")
+
+@WebServlet("/voorstellingen.htm")
 public class Voorstellingen extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/voorstellingen.jsp";
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Voorstellingen() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.getRequestDispatcher(VIEW).forward(request, response); 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
